86dd1fca6313ad3180547e5282daae92d9fa2470
王 <c18519667281@gmail.com>
2022-05-09 09:43:25 +0800

    Update SAV_MailBox.cs

--- PKHeX.WinForms/Subforms/Save Editors/SAV_MailBox.cs
+++ PKHeX.WinForms/Subforms/Save Editors/SAV_MailBox.cs
@@ -76,7 +76,7 @@ namespace PKHeX.WinForms
                 case SAV4 sav4:
                     m = new Mail4[p.Count + 20];
                     for (int i = 0; i < p.Count; i++)
-                        m[i] = new Mail4(((PK4)p[i]).GetHeldMailData());
+                        m[i] = new Mail4(((PK4)p[i]).HeldMail.ToArray());
                     for (int i = p.Count, j = 0; i < m.Length; i++, j++)
                         m[i] = sav4.GetMail(j);
                     var l4 = (Mail4)m[^1];
